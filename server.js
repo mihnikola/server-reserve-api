38f07f4337f4d71bd04f1d84350fcf62f2d8bd17
mihnikola <mihailovic93.nikola@gmail.com>
2025-03-09 20:42:50 +0100

    deploy

--- server.js
+++ server.js
@@ -166,7 +166,9 @@ app.post("/api/send-notification", async (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports = app
